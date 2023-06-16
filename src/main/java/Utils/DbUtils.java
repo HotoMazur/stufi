9ccb04f74ad9f16813ab738b9758e2fec35e5066
manchkinq <129665007+manchkinq@users.noreply.github.com>
2023-06-16 16:03:56 +0300

    main

--- src/main/java/Utils/DbUtils.java
+++ src/main/java/Utils/DbUtils.java
@@ -1,4 +1,36 @@
 package Utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DbUtils {
+    public static Connection connectToDb() {
+        final String url = "jdbc:postgresql://db.fxnuxdddwkrgwubkaocg.supabase.co:5432/postgres";
+        final String user = "postgres";
+        final String password = "dimastest23445870";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+
+
+    public static void disconnect(Connection connection, PreparedStatement preparedStatement) {
+        try {
+            if (connection != null && preparedStatement != null) {
+                connection.close();
+                preparedStatement.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
