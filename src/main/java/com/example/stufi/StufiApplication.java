0d6a6a54a5848b22b599179081d378205fce2cff
manchkinq <129665007+manchkinq@users.noreply.github.com>
2023-06-16 15:46:39 +0300

    main

--- src/main/java/com/example/stufi/StufiApplication.java
+++ src/main/java/com/example/stufi/StufiApplication.java
@@ -9,6 +9,9 @@ public class StufiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StufiApplication.class, args);
 		System.out.println("Hello, Diman!");
+		int a = 5;
+		int b = 10;
+		System.out.println(a + b);
 	}
 
 }
