d01bb0588bc374ecb5949f68f902ab7573fdca86
Dmytro Mazurev <dmytromazurev@Dmytros-MacBook-Pro.local>
2023-06-16 14:02:35 +0300

    Initial commit

---
+++ src/main/java/com/example/stufi/StufiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stufi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StufiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StufiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stufi/StufiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stufi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StufiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
