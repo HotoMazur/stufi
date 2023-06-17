46e4e407db914593989d329e3863acbe1a166972
Danylo Filatov <danylo_filatov@epam.com>
2023-06-17 13:25:02 +0300

    User updated all

--- src/main/java/com/example/stufi/User/User.java
+++ src/main/java/com/example/stufi/User/User.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 @Getter
 @Setter
@@ -14,30 +13,33 @@ import java.util.ArrayList;
 @NoArgsConstructor
 public class User {
     private double id;
-    private String username; // unique
     private String password;
     private String name;
     private String surname;
     private String email;
     private LocalDate birthDate;
+    private String uni_name;
+    private String faculty;
+    private String major;
+    private String group;
+    private LocalDate created_at;
     private String phoneNumber;
     private LocalDate startStudyDate;
     private LocalDate endStudyDate;
-    private boolean sex; // 1 is boy, 0 is girl
-    private String[] languages;
-    private LocalDate createAt;
 
-    public User(String username, String password, String name, String surname, String email, LocalDate birthDate, String phoneNumber, LocalDate startStudyDate, LocalDate endStudyDate, boolean sex, String[] languages) {
-        this.username = username;
+    public User(String password, String name, String surname, String email, LocalDate birthDate, String uni_name, String faculty, String major, String group, LocalDate created_at, String phoneNumber, LocalDate startStudyDate, LocalDate endStudyDate) {
         this.password = password;
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.birthDate = birthDate;
+        this.uni_name = uni_name;
+        this.faculty = faculty;
+        this.major = major;
+        this.group = group;
+        this.created_at = created_at;
         this.phoneNumber = phoneNumber;
         this.startStudyDate = startStudyDate;
         this.endStudyDate = endStudyDate;
-        this.sex = sex;
-        this.languages = languages;
     }
 }
--- src/main/java/com/example/stufi/User/UserDao.java
+++ src/main/java/com/example/stufi/User/UserDao.java
@@ -9,22 +9,33 @@ import java.util.ArrayList;
 public class UserDao {
     public static User addUserToDb() throws SQLException {
         String[] language ={"en"};
-        User newUser = new User("Maz", "1234", "Dima", "Mazurev", "dmytro.mazurev", LocalDate.now(), "12312312", LocalDate.now(),  LocalDate.now(), true, language);
+        User newUser = new User("1234",
+                "Dmytro",
+                "Mazurev",
+                "yttyt@gmai.com",
+                LocalDate.now(),
+                "VGTU",
+                "FS",
+                "IT",
+                "ITfuc",
+                LocalDate.now(),
+                "12131213",
+                LocalDate.now(),
+                LocalDate.now());
         Connection connection = DbUtils.connectToDb();
-        PreparedStatement ps = connection.prepareStatement("insert into \"user\" (name, surname, email, birth_date, phone_number, start_study_date, end_study_date, sex, languages, password, username) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
-        ps.setString(1, newUser.getName());
-        ps.setString(2, newUser.getSurname());
-        ps.setString(3, newUser.getEmail());
-        ps.setDate(4, Date.valueOf(newUser.getBirthDate()));
+        PreparedStatement ps = connection.prepareStatement("insert into \"users\" (password,name,surname,email,phone_number,birth_date,uni_name,faculty,major,group,start_study_date,end_study_date,created_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+        ps.setString(1, newUser.getPassword());
+        ps.setString(2, newUser.getName());
+        ps.setString(3, newUser.getSurname());
+        ps.setString(4,newUser.getEmail());
         ps.setString(5, newUser.getPhoneNumber());
-        ps.setDate(6, Date.valueOf(newUser.getStartStudyDate()));
-        ps.setDate(7, Date.valueOf(newUser.getEndStudyDate()));
-        ps.setBoolean(8, newUser.isSex());
-        Array array = connection.createArrayOf("varchar", language);
-        ps.setArray(9, array);
-        ps.setString(10, newUser.getPassword());
-        ps.setString(11, newUser.getUsername());
-
+        ps.setDate(6, Date.valueOf(newUser.getBirthDate()));
+        ps.setString(7,newUser.getUni_name());
+        ps.setString(8,newUser.getFaculty());
+        ps.setString(9,newUser.getGroup());
+        ps.setDate(10, Date.valueOf(newUser.getStartStudyDate()));
+        ps.setDate(11, Date.valueOf(newUser.getEndStudyDate()));
+        ps.setDate(12,Date.valueOf(newUser.getCreated_at()));
         ps.execute();
         return null;
     }
